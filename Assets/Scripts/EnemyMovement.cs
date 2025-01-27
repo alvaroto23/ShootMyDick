f1acca1ead2c0c384993c24c0298b06c0ad4407e
Álvaro Ruiz <alvaro231099@gmail.com>
2025-01-27 16:42:55 +0100

    Set AI of enemy but not finished

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform target;
+
+    private NavMeshAgent agent;
+
+
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
